d635d4e85c1ac8e196657706883ec965335705af
murakami-miwako <murakami_miwako@cyberagent.co.jp>
2019-07-22 11:46:54 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+pkg-mvn# pkg-mvn
---
+++ src/com/redsyrup/test/TestPkgRegistory.java
@@ -0,0 +1,7 @@
+package com.redsyrup.test;
+
+public class TestPkgRegistory {
+    public static void man (String[] args) {
+        Sytem.out.println("Test");
+    }
+}
